22a6d6a302632c8649b673a3905f5f77c9f6c686
Andreas Engl <ae@cephea.de>
2016-11-30 14:18:34 +0100

    Unit test for JsCode

--- src/jscode.test.tsx
+++ src/jscode.test.tsx
@@ -1,21 +1,22 @@
-import {VariableDeclaration, Literal, Identifier} from './jscode'
-
-import {GenericJsNode} from './jsnode';
-import * as ast from 'ast-types';
+import { VariableDeclaration, Literal, Identifier } from './jscode';
+import { JsNode, GenericJsNode } from './jsnode';
 import { JsBuilder , VariableKind} from './jsbuilder';
+import * as ast from 'ast-types';
 
-class React {
-
+class JsCode {
   static createElement(...args: any[]): GenericJsNode {
     let [func, props, ...children] = args;
     return new args[0](args[1], children);
   }
 }
 
-
 describe('jsnodex', () => {
-    it('test', () => {
-      let foo = <VariableDeclaration name="foobar"><Literal value="3"/><Identifier value="3"/></VariableDeclaration> as VariableDeclaration
-    });
-
+  it('test', () => {
+    let foo = (
+      <VariableDeclaration name='foobar'>
+        <Identifier value='42'/>
+      </VariableDeclaration>
+    ) as VariableDeclaration;
+    expect(foo.format()).toBe('let foobar = 42;');
   });
+});
