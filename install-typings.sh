3cadb28fbd6824e650c5ac02399a3efbcbf471e4
Andreas Engl <ae@cephea.de>
2016-12-01 12:45:40 +0100

    Minor correction

--- install-typings.sh
+++ install-typings.sh
@@ -3,5 +3,5 @@
 mkdir node_modules/@types
 mkdir node_modules/@types/ast-types
 cp typings/ast-types.d.ts node_modules/@types/ast-types/index.d.ts
-mkdir node_modules/@types/jscodeshift
-cp typings/jscodeshift.d.ts node_modules/@types/jscodeshift/index.d.ts
+#mkdir node_modules/@types/jscodeshift
+#cp typings/jscodeshift.d.ts node_modules/@types/jscodeshift/index.d.ts
