0cf9b7a1862dbd91d85bce93e92b4875b08dab4d
Blair Harris <beh@trifork.com>
2016-12-01 13:42:36 +0100

    updated Makefile

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
 build:
+	./install-typings.sh
 	tsc
 
 setup:
--- install-typings.sh
+++ install-typings.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-mkdir node_modules/@types
-mkdir node_modules/@types/ast-types
+if [  ! -d "node_modules/@types" ]; then
+  mkdir node_modules/@types
+fi
+if [ ! -d "node_modules/@types/ast-types" ]; then
+  mkdir node_modules/@types/ast-types
+fi
 cp typings/ast-types.d.ts node_modules/@types/ast-types/index.d.ts
 #mkdir node_modules/@types/jscodeshift
 #cp typings/jscodeshift.d.ts node_modules/@types/jscodeshift/index.d.ts
