6ea5ef9030385f1668df35752489a40323cd4c50
Andreas Engl <ae@cephea.de>
2016-12-08 17:25:26 +0100

    Added demo for showing off transforms

---
+++ src/demo/editor.ts
@@ -0,0 +1,20 @@
+import { Transformation } from '../Transformation';
+import { GenericJsNode, NamedTypes as t } from '../JsNode';
+import { Identifier } from 'ast-types';
+import { Project } from '../Project';
+
+export class ExampleEditor implements Transformation {
+  constructor() {
+  }
+
+  configure(args: any[]): void {
+  }
+
+  check(root: GenericJsNode, project: Project): boolean {
+    return true;
+  }
+
+  apply(root: GenericJsNode, project: Project): GenericJsNode {
+    return root;
+  }
+}
