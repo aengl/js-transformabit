8e5e64e6d3be04c191961a2389cadcc8ca6d0ef4
Andreas Engl <ae@cephea.de>
2016-12-19 15:01:09 +0100

    Created branch for testing against Nashorn

---
+++ nashorn/src/nashorn/Nashorn.java
@@ -0,0 +1,13 @@
+package nashorn;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+
+public class Nashorn {
+  public static void main(String... args) throws Throwable {
+    ScriptEngineManager engineManager = new ScriptEngineManager();
+    ScriptEngine engine = engineManager.getEngineByName("nashorn");
+    engine.eval("function sum(a, b) { return a + b; }");
+    System.out.println(engine.eval("sum(1, 2);"));
+  }
+}
