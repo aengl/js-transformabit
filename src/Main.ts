5e80988137bac23fc971e5d0e26fc0a36f1f432c
Andreas Engl <ae@cephea.de>
2016-12-21 11:32:00 +0100

    Moved demos and transformations to another repo

--- src/Main.ts
+++ src/Main.ts
@@ -1,2 +1,5 @@
 export * from './JsNode';
 export * from './JsCode';
+export * from './Project';
+export * from './Transformation';
+export * from './TestSuiteRunner';
--- src/Transformation.ts
+++ src/Transformation.ts
@@ -3,6 +3,6 @@ import { Project } from './Project';
 
 export interface Transformation {
   configure(...args: any[]): void;
-  check(root: GenericJsNode, project: Project): boolean;
+  check?(root: GenericJsNode, project: Project): boolean;
   apply(root: GenericJsNode, project: Project): GenericJsNode;
 }
